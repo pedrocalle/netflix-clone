31fcb56e57201e3c9c2a2ed00ea006a15e7b04b9
Pedro Calle <ph.calle@hotmail.com>
2023-07-19 18:21:35 -0300

    FIrst Commit Netflix

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+export function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_content">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
+          alt=""
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCCkd4U1A2Ku8QWFLKwKFtrclXui_gY78",
+  authDomain: "netflix-clone-89ed5.firebaseapp.com",
+  projectId: "netflix-clone-89ed5",
+  storageBucket: "netflix-clone-89ed5.appspot.com",
+  messagingSenderId: "103396547444",
+  appId: "1:103396547444:web:8477ec2ff918a3f66cea21",
+};
